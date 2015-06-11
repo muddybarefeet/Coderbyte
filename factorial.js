f24e4c04ba0dc6b388246bab9a7495487f614e48
Anna Rogers <2muddybarefeet@gmail.com>
2015-06-11 07:37:19 +0100

    Added factorial solution

---
+++ factorial.js
@@ -0,0 +1,23 @@
+
+// I got there finally... with some Rohan help..
+
+
+var factorial = function(n){
+    var result=0;
+    if(n === 0){
+        return 0;
+    }else{
+        result = 1;
+    }
+    for(var i = 1; i<=n; i++) {
+        result = result * i;
+    }
+    return result;
+};
+
+
+factorial(4);
+
+
+
+
