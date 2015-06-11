c8ae7bdf3cb434774efeb211331d79af2efcf499
Anna Rogers <2muddybarefeet@gmail.com>
2015-06-11 09:27:52 +0100

    Add proper description

--- firstReverse.js
+++ firstReverse.js
@@ -1,32 +1,5 @@
 
-
-var love = 'I love you more!';
-
-var surprise = function (more) {
-    var ni = more
-             .split('')
-             .reverse()
-             .join('');
-             
-    return ni;
-};
-
-surprise(love);
-
-// or
-
-var love = 'I love you more!';
-
-var surprise = function (more) {
-    return more.split('').reverse().join('');
-};
-
-surprise(love);
-
-
-
-
-// coderbyte answer
+/*take a string and reverse it*/
 
 function FirstReverse(str) {
   return str.split('').reverse().join('');
