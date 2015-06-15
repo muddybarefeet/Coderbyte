26ef3aa81771a5018f88c181cd7fa31985995ea8
Anna Rogers <2muddybarefeet@gmail.com>
2015-06-15 09:31:59 +0100

    dashInsert Solution

---
+++ dashInsert.js
@@ -0,0 +1,22 @@
+//put dashes between odd numbers
+
+var num = 37384904;
+
+var dashInsert = function(str) {
+	var array = str.toString();
+	var result="";
+	for(var i=0; i<array.length; i++)	{
+		if(array[i]%2 !== 0 && array[i+1]%2 !== 0) { //if odd and the one after is odd add a dash after the current element in result
+			result = result + array[i] + "-";
+		}else {
+			result = result + array[i]; //else add element to result
+		}
+	}
+    if(result[result.length-1] === '-') { result = result.slice(0,-1); } //if the last one is a dash remove it!
+	return result.replace(/-+/g, '-'); //returns a string with multiple '-'s removed
+};
+
+dashInsert(num);
+
+/*can't see why it is not working with some numbers?? Especialy many of the same number...*/
+
