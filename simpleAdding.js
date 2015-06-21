7d1612d354edb49da4118cbeb38147a702021490
Anna Rogers <2muddybarefeet@gmail.com>
2015-06-21 09:28:17 +0100

    simpleAdding Solution

---
+++ simpleAdding.js
@@ -0,0 +1,17 @@
+
+//take argument and add up all numbers from one to that numbers
+
+var arr = [9];
+
+var simpleAdding = function (num) {
+	var result = 0;
+	for(var i=1; i<=num; i++) {
+		result= result + i;
+	}
+	return result;
+};
+
+simpleAdding(arr);
+
+//REMEMBER TO ADD ALL THE I NOT NUM(NUM[I])!!
+
