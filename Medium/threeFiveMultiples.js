46e74d41f72f655e9d4ce56789c12364fc4f83d0
Anna Rogers <2muddybarefeet@gmail.com>
2015-08-15 13:40:53 -0700

    threeFiveMultiples Solution

---
+++ Medium/threeFiveMultiples.js
@@ -0,0 +1,19 @@
+/*
+Using the JavaScript language, have the function ThreeFiveMultiples(num) return the sum of all the multiples 
+of 3 and 5 that are below num. For example: if num is 10, the multiples of 3 and 5 that are below 10 are 3, 5, 6, 
+and 9, and adding them up you get 23, so your program should return 23. The integer being passed will be between 1 and 100.
+
+Input = 6Output = 8
+Input = 1Output = 0
+*/
+
+
+var ThreeFiveMultiples = function(num) {
+	var sum = 0;
+	for (var i = 0; i < num; i++) {
+		if(i % 3 === 0|| i%5 === 0)
+			result+=i;
+	}
+	return result;
+};
+ThreeFiveMultiples(10);
