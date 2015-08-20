ff55178261089c1c6bf8b507010a6459e2cd58a6
Anna Rogers <2muddybarefeet@gmail.com>
2015-08-20 08:34:08 -0700

    primeMover Optimisations

--- Medium/primeMover.js
+++ Medium/primeMover.js
@@ -5,10 +5,18 @@ Input = 9 Output = 23
 Input = 100 Output = 541
 */
 
-var primeTime = function(num) { // From previous challenge
-  if(num < 2) return false;
-  for(var i = 2; i < num; i++){
-    if(num % i === 0) return false;
+var primeTester = function(num) {
+  if(num<2) {
+    return false;
+  }
+  var limit = Math.ceil(Math.sqrt(num)); //optomises the function by liminting the number of loops :)
+  if(num%2===0&& num!==2) {
+    return false;
+  }
+  for(var i=3; i<=limit; i++) {
+    if(num%i===0) {
+      return false;
+    }
   }
   return true;
 };
@@ -16,9 +24,23 @@ var primeTime = function(num) { // From previous challenge
 var primeMover = function(num) {
   var primeCount = [];
   for(var i = 0; i < 10000; i++){
-    if(PrimeTime(i)) {
+    if(primeTester(i)) {
     	primeCount.push(i);
     }
   }
   return primeCount[num-1];
+};
+
+//extra question
+
+//find all the number in the range that are primes and return
+
+var primeSieve = function(start, end) {
+  var result = [];
+  for(var i=start; i<=end; i++) {
+    if(primeTester(i)) {
+      result.push(i);
+    }
+  }
+  return result;
 };
